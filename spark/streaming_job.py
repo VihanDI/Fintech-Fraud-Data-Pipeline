3a54093b8d9aa1e29024d3a4f0618961f132459e
Inupa Vihan <inupavihan9@gmail.com>
2026-03-20 17:49:37 +0530

    Add Spark steaming job
    Update docker-compose file

---
+++ spark/streaming_job.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+# creating the Spark session
+spark = SparkSession.builder.appName("KafkaSparkStreaming").getOrCreate()
+
+# reading from Kafka
+df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka:9092").option("subscribe", "transactions").option("startingOffsets", "latest").load()
+
+# converting value to string
+df = df.selectExpr("CAST(value AS STRING)")
+
+# printing to the console
+query = df.writeStream.outputMode("append").format("console").start()
+
+query.awaitTermination()
