2043bbc7319c045c91d539e2f11d3f681126d54b
Inupa Vihan <inupavihan9@gmail.com>
2026-03-25 17:07:35 +0530

    Update streaming job code
    Add functionality to write into the database

--- spark/streaming_job.py
+++ spark/streaming_job.py
@@ -1,6 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json, when
+from pyspark.sql.functions import concat_ws
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, IntegerType, ArrayType
+
+def write_to_postgres(batch_df, batch_id):
+    batch_df.write.format("jdbc").option("url", "jdbc:postgresql://postgres:5432/airflow").option("dbtable", "transactions").option("user", "airflow").option("password", "airflow").option("driver", "org.postgresql.Driver").mode("append").save()
 
 schema = StructType([
     StructField("transaction_id", StringType(), True),
@@ -24,7 +28,12 @@ parsed_df = df.selectExpr("CAST(value AS STRING)").select(from_json(col("value")
 # adding fraud detection rule
 fraud_df = parsed_df.withColumn("fraud_flag", when(col("amount") > 3000, "high_value").otherwise("normal"))
 
+fraud_df = fraud_df.withColumn("category", concat_ws(",", col("category")))
+
 # printing to the console
-query = fraud_df.writeStream.format("console").option("truncate", "false").outputMode("append").start()
+# query = fraud_df.writeStream.format("console").option("truncate", "false").outputMode("append").start()
+
+# writing to the database
+query = fraud_df.writeStream.foreachBatch(write_to_postgres).outputMode("append").start()
 
 query.awaitTermination()
